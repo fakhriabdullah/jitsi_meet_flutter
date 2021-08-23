99b17be4765021ff969390a3fb461d088ce43078
fakhriabdullah <fakhri.abdullah9496@gmail.com>
2021-08-23 16:03:38 +0700

    update audo only

--- ios/Classes/JitsiViewController.swift
+++ ios/Classes/JitsiViewController.swift
@@ -68,9 +68,9 @@ class JitsiViewController: UIViewController {
             builder.serverURL = self.serverUrl
             // builder.subject = self.subject
             builder.userInfo = self.jistiMeetUserInfo
-            builder.audioOnly = self.audioOnly ?? false
-            builder.audioMuted = self.audioMuted ?? false
-            builder.videoMuted = self.videoMuted ?? false
+            // builder.audioOnly = self.audioOnly ?? false
+            // builder.audioMuted = self.audioMuted ?? false
+            // builder.videoMuted = self.videoMuted ?? false
             builder.token = self.token
             
             self.featureFlags?.forEach{ key,value in
